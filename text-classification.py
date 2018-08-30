a7ed511e584dc71c63b063f89467f7c329b40310
Guilherme Medina <guilherme_abinader@hotmail.com>
2018-08-30 16:56:07 -0300

    Print image with text

--- text-classification.py
+++ text-classification.py
@@ -14,6 +14,11 @@ elif k == ord('s'): # wait for 's' key to save and exit
     cv2.destroyAllWindows()
     exit()
 
-plt.imshow(img, cmap = "gray", interpolation = "bicubic")
-plt.xticks([]), plt.yticks([])
-plt.show()
+# plt.imshow(img, cmap = "gray", interpolation = "bicubic")
+# plt.xticks([]), plt.yticks([])
+# plt.show()
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+cv2.putText(img,"Here's looking at you, kid",(10,100), font, 1, (255,255,255), 2, cv2.LINE_AA)
+cv2.imshow('casablanca with text',img)
+cv2.waitKey(0)
