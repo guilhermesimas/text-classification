36b48a3b91319f0103fe382069974d87ebed4ba9
Guilherme Medina <guilherme_abinader@hotmail.com>
2018-08-30 14:46:51 -0300

    Open image and produce copy

--- text-classification.py
+++ text-classification.py
@@ -1,2 +1,16 @@
 import cv2
-print(cv2.__version__)
+
+print("OpenCV Python Tutorial using version " + cv2.__version__)
+
+img = cv2.imread("img.pgm",cv2.IMREAD_GRAYSCALE)
+
+cv2.imshow('casablanca',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+cv2.namedWindow("casablanca",cv2.WINDOW_NORMAL)
+cv2.imshow('casablanca',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+cv2.imwrite("img.png",img)
