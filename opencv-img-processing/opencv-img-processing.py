2677f90cef387c03fac97a4c1cb31fe092748888
Guilherme Medina <guilherme_abinader@hotmail.com>
2018-08-31 10:09:12 -0300

    Color image separate in channels

--- opencv-img-processing/opencv-img-processing.py
+++ opencv-img-processing/opencv-img-processing.py
@@ -14,3 +14,22 @@ img_partial = img[int(width*1/4):int(width*3/4),int(height*1/4):int(height*3/4)]
 
 cv2.imshow("partial image",img_partial)
 cv2.waitKey(0)
+
+img_color = cv2.imread("color.tiff")
+cv2.imshow("Peppers",img_color)
+cv2.waitKey(0)
+
+b, g, r = cv2.split(img_color)
+b.fill(0)
+g.fill(0)
+
+cv2.imshow("Red",cv2.merge((b,g,r)))
+cv2.waitKey(0)
+
+cv2.imshow("Green channel",img_color[:,:,1])
+cv2.waitKey(0)
+
+img_color[:,:,1] = 0
+img_color[:,:,2] = 0
+cv2.imshow("Blue",img_color)
+cv2.waitKey(0)
